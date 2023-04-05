a3c490636b0ee9157e19cc12cdaf614d0e8878f1
d0wnsider <jmhandsamson@gmail.com>
2023-04-04 23:16:04 -0400

    Add event listeners

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
-const Gameboard = { gameboard: ["X", "O", "X", "O", "X", "O"] };
+// * module(IIFE) for for single purpose functions/ factories for multiples like players
+//* gameboard
+//TODO
+const gameboard = { tile: ["", "", "", "", "", "", "", "", ""] };
+// adding event listener
+const tile = document.querySelectorAll(".tile");
+tile.forEach((tile) => {
+  tile.addEventListener("click", (e) => {
+    tile.textContent = player1.getSymbol();
+  });
+});
 
-function displayController() {
-  const boardDisplay = document.querySelector(".gameboard");
+//* players
+// what should players do?
+const Player = (name, symbol) => {
+  const getName = () => name;
+  const getSymbol = () => symbol;
+  return { getName, getSymbol };
+};
 
-  boardDisplay.textContent = Gameboard.gameboard;
-  
-}
+const player1 = Player("Player 1", "X");
+const player2 = Player("Player 2", "O");
+
+//TODO
+const display = () => {};
+
+//TODO gameflow sequence
+const game = () => {};
+
+//* starts the game
+//TODO starts the game when start button is clicked
+game();
